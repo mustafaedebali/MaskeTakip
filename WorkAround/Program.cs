cf0949a24185ee730be639bca54c2443f777d52b
Mustafa Edebalı <github.i1pbm@silomails.com>
2024-02-06 06:05:22 +0300

    Initial commit

---
+++ WorkAround/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+
+Person person1 = new Person();
+person1.FirstName = "İsim1";
+person1.LastName = "Soyisim1";
+person1.DateOfBirthYear = 1995;
+person1.NationalIdentity = 123;
+
+Person person2 = new Person();
+person1.FirstName = "İsim2";
+person1.LastName = "Soyisim2";
+person1.DateOfBirthYear = 1995;
+person1.NationalIdentity = 123;
+
+
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
